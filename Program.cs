38bab9fb1386caa08c50cfeadd3e223f925f7a03
vision800 <netshokotshokovision112@gmail.com>
2024-01-10 10:17:17 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _17.The_for_Loop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("the loop for reference");
+            int i = 0;
+            while (i <=5)
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+
+            Console.WriteLine("the for loop");
+
+            for( i=0; i<5; i++)
+            {
+                Console.WriteLine(i);
+            }
+            Console.Read();
+        }
+    }
+}
